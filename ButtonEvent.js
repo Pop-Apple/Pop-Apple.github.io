fecf0430a5017d6be0f6fdc56b4ea8e72141f583
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2022-08-16 23:27:17 +0900

    Add files via upload

---
+++ ButtonEvent.js
@@ -0,0 +1,10 @@
+function Connect2Github() {
+    window.location.href = 'https://github.com/Pop-Apple'
+}
+
+function HappyBirthday() {
+    window.location.href = 'https://pop-apple.github.io/Happy-Birthday-Friend/index.html'
+    }
+function Connect2Twitter() {
+    window.location.href = 'https://twitter.com/4pple_kun'
+}
