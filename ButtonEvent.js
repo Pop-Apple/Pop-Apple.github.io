21275aefbf474e49ab48cdec66a70496b81d8845
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2022-08-18 20:38:21 +0900

    Update ButtonEvent.js

--- ButtonEvent.js
+++ ButtonEvent.js
@@ -3,8 +3,9 @@ function Connect2Github() {
 }
 
 function HappyBirthday() {
-    window.location.href = 'https://pop-apple.github.io/Happy-Birthday-Friend/index.html'
+    window.location.href = 'https://pop-apple.github.io/Happy-Birthday-Friend/'
     }
+
 function Connect2Twitter() {
     window.location.href = 'https://twitter.com/4pple_kun'
 }
