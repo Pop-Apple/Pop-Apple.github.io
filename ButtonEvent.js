1449898d4231c7e4763f8411e5bf6da1e1f68fc9
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2022-08-22 12:51:58 +0900

    Add files via upload

---
+++ ButtonEvent.js
@@ -0,0 +1,11 @@
+function Connect2Github() {
+    window.location.href = 'https://github.com/Pop-Apple'
+}
+
+function Connect2MoreFriend() {
+    window.location.href = 'Pop-Apple.github.io-main/Friendly/'
+    }
+
+function Connect2Twitter() {
+    window.location.href = 'https://twitter.com/4pple_kun'
+}
---
+++ Friendly/Friendly.js
@@ -0,0 +1,3 @@
+function TakaHub() {
+    window.location.href = 'Pop-Apple.github.io-main/Friendly/Taka/index.html'
+}
