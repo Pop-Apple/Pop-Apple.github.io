71cf68227e5922ee8a7b253d1d86980ece2d659f
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2022-08-22 12:50:37 +0900

    Delete Happy-Birthday-Friend directory

--- Happy-Birthday-Friend/ButtonEvent.js
+++
@@ -1,7 +0,0 @@
-function Connect2Twitter() {
-    window.location.href = 'https://twitter.com/Taka45248709'
-}
-
-function Connect2Youtube() {
-    window.location.href = 'https://www.youtube.com/channel/UCBURjLg5ajb3HKVnZNy1T-g'
-}
