b9cae7894e275df13f93f093999d351a9acf7b46
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2022-09-14 23:30:47 +0900

    js Function

---
+++ Cords/buttonEvent.js
@@ -0,0 +1,25 @@
+/*
+* Javascript Button Evento
+*/
+function Menu() {
+    window.location.href = 'https://pop-apple.github.io/'
+}
+function Twitter() {
+    window.location.href = 'https://twitter.com/4pple_kun'
+}
+function Github() {
+    window.location.href = 'https://github.com/Pop-Apple'
+}
+function Contact() {
+    window.location.href = ''
+}
+function Repo1() {
+    window.location.href = 'https://github.com/Pop-Apple/Minecraft-WiiU-Pixel-Art-Tool'
+}
+/*
+* Warning Alert
+*/
+function Alert1() {
+    var MessageBox = "This page has not been updated"
+    alert(MessageBox)
+}
