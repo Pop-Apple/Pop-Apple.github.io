aec443209008d04fd1e5c16de30349ff6e9d154c
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2023-02-11 21:28:19 +0900

    Added Project

---
+++ script.js
@@ -0,0 +1,30 @@
+function Github() {
+
+    const GithubURL = 'https://github.com/Pop-Apple'
+    window.open(GithubURL, '_blank')
+
+    console.log('Successfully')
+
+}
+
+function Twitter() {
+
+    const TwitterURL = 'https://twitter.com/4pple_kun'
+    window.open(TwitterURL, '_blank')
+
+    console.log('Successfully')
+
+}
+
+function Alert() {
+
+    var Message = 'Not Found 404 Error'
+    
+    const InstagramURL = 'https://instagram.com/sakamochan123/'
+    window.open(InstagramURL, '_blank')
+    
+    console.log('Successfully')
+    
+    //alert(Message)
+
+}
